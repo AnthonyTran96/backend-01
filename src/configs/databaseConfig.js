a6aed62c55f9b7644e45d51f8298bd430e70e767
AnthonyTran <quanghust96@gmail.com>
2023-05-17 01:31:27 +0700

    chinh sua lai chut

--- src/configs/databaseConfig.js
+++ src/configs/databaseConfig.js
@@ -7,7 +7,6 @@ const connection = async ()=>{
         pass: process.env.DB_PASS,
         dbName: process.env.DB_NAME,
     };
-    console.log('>>>check env: ', options, process.env.DB_HOST)
     await mongoose.connect(process.env.DB_HOST, options);
 }
 
--- src/configs/templateEngineConfig.js
+++ src/configs/templateEngineConfig.js
@@ -5,8 +5,7 @@ const templateEngineConfig = (app)=>{
     //config template engine
     app.set('view engine', "ejs");
     app.set('views', path.resolve(__dirname, "../views"));
-    console.log(path.resolve(__dirname, "../public"));
-
+    
     //config static file
     app.use(express.static(path.join(__dirname, '../public')));
 }
